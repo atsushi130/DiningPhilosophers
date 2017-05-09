4fc952dd88f90cf4742dcdf0b5d19dc20dffc663
atsushi130 <atsushi_main@outlook.jp>
2017-05-09 10:48:44 +0900

    the dining philosopher model

---
+++ src/philosopher.rs
@@ -0,0 +1,36 @@
+
+/**
+ * 食事する哲学者モデル
+ * Created by Atsushi Miyake on 2017/05/09.
+ * Copyright © 2017年 Atsushi Miyake. All rights reserved.
+ **/
+
+use std::thread;
+use std::time::Duration;
+use table::Table;
+
+pub struct Philosopher {
+    name: String,
+    left: usize,
+    right: usize
+}
+
+impl Philosopher {
+    pub fn new(name: &str, left: usize, right: usize) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+            left: left,
+            right: right
+        }
+    }
+
+    pub fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        thread::sleep(Duration::from_millis(150));
+        let _right = table.forks[self.right].lock().unwrap();
+
+        println!("{} is eating.", self.name);
+        thread::sleep(Duration::from_millis(1000));
+        println!("{} is done eating.", self.name);
+    }
+}
