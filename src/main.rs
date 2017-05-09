3295048b4c51967be553890212c1450d625fe860
atsushi130 <atsushi_main@outlook.jp>
2017-05-09 10:48:58 +0900

    add main.rs

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+
+/**
+ * main.rs
+ * Created by Atsushi Miyake on 2017/05/09.
+ * Copyright © 2017年 Atsushi Miyake. All rights reserved.
+ **/
+
+use std::thread;
+use std::sync::{Mutex, Arc};
+
+mod philosopher;
+mod table;
+
+fn main() {
+    let table = Arc::new(table::Table { forks: vec![
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(())
+    ]});
+
+    let philosophers = vec![
+        philosopher::Philosopher::new("Judith Butler", 0, 1),
+        philosopher::Philosopher::new("Gilles Deleuze", 1, 2),
+        philosopher::Philosopher::new("Karl Marx", 2, 3),
+        philosopher::Philosopher::new("Emma Goldman", 3, 4),
+        philosopher::Philosopher::new("Michel Foucault", 0, 4)
+    ];
+
+    let handles: Vec<_> = philosophers.into_iter().map(|p| {
+        let table = table.clone();
+        thread::spawn(move || {
+            p.eat(&table);
+        })
+    }).collect();
+
+    for h in handles {
+        h.join().unwrap();
+    }
+}
